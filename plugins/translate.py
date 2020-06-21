c165535db3ba5b489939b2c172f5f62b881bc940
lv101 <lv101@users.noreply.github.com>
2020-06-21 12:39:34 +0800

    Update translate.py

--- plugins/translate.py
+++ plugins/translate.py
@@ -1,4 +1,3 @@
-import re
 import requests
 from nonebot import on_command, CommandSession
 
@@ -7,22 +6,33 @@ command = ['翻译', 'translate', 'fy']
 
 @on_command('fanyi', aliases=command, only_to_me=False)
 async def fanyi(session: CommandSession):
-    arg = session.current_arg_text.strip()
+    arg = session.current_arg_text
+    print(arg)
+
+    at = ''
+    if session.ctx['message_type'] == 'group':
+        at = f"[CQ:at,qq={session.ctx['user_id']}] "
 
     if not arg:
-        await session.pause('人家没有看到要翻译的内容呢,再试一次吧 >-<')
+        await session.pause(at+'请输入你想要翻译的内容 >-<')
 
     paylod = {'doctype': 'json',
               'type': 'AUTO',
               'i': arg}
     url = 'http://fanyi.youdao.com/translate'
-    r = requests.get(url, params=paylod)
-    type_ = re.findall(r'"type":(.*?),', r.text)[0]
-    error = re.findall(r'"errorCode":(.*?),', r.text)[0]
-    text = re.findall(r'"tgt":"(.*?)"', r.text)[0]
-    if eval(type_) == "UNSUPPORTED":
-        await session.send("这个词好深奥,人家不会呢,换个词试试吧 >_<")
+    data = requests.get(url, params=paylod).json()
+
+    # print(r.url)
+    type_ = data["type"]
+    error = data["errorCode"]
+    results = data["translateResult"]
+    if type_ == "UNSUPPORTED":
+        await session.send(at+"这个词好深奥,人家不会呢,换个词试试吧 >_<")
     else:
-        if session.ctx['message_type'] == 'group':
-            text = f"[CQ:at, qq={session.ctx['user_id']}] " + text
-        await session.send(text)
+        trans = []
+        for result in results:
+            text = result[0]["tgt"]
+            if text:
+                trans.append(text)
+        result_info = '\n'.join(trans)
+        await session.send(result_info)
